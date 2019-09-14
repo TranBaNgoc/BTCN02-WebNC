c4941391915a7f8d2883ffce519908aff9a4492c
TranBaNgoc <1612431@student.hcmus.edu.vn>
2019-09-14 17:49:08 +0700

    Cap nhat trang thai chien thang

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import './style.css'
-import './App.css';
+import './style.css';
+
+const MaxHeight = 20;
+const MaxWidth = 20;
+
+var value = -1;
 
 function Square(props) {
   return (
@@ -17,6 +21,7 @@ class Board extends React.Component {
     this.state = {
       squares: Array(400).fill(null),
       xIsNext: true,
+      isEnded: false,
     };
   }
 
@@ -31,9 +36,12 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (calculateWinner(this.state.squares) && squares[i]) {
+    
+    if (this.state.isEnded || squares[i]) {
       return;
     }
+
+    value = i;
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -41,24 +49,33 @@ class Board extends React.Component {
     });
   }
 
-  handleClickReset(){
+  handleClickReset() {
     this.setState({
       squares: Array(400).fill(null),
       xIsNext: true,
+      isEnded: false,
     });
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      this.state.isEnded = true;
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     let board = [];
     for (let i = 0; i < 20; i++) {
       let rowBoard = [];
       for (let j = 0; j < 20; j++) {
-        rowBoard.push(this.renderSquare(i*20 + j))    
+        rowBoard.push(this.renderSquare(i * 20 + j))
       }
       board.push(<div className="board-row">
-         {rowBoard}
+        {rowBoard}
       </div>)
     }
 
@@ -90,7 +107,133 @@ class Game extends React.Component {
 }
 
 function calculateWinner(squares) {
-  return !null;
+
+  if (value === -1) {
+    return null;
+  }
+
+  var row = Math.floor(value / 20);
+  var column = value % 20;
+
+  var thisValue = squares[row * 20 + column];
+  // Kiểm tra hàng dọc chứa điểm vừa đánh
+  for (var index = row - 4; index <= row; index++) {
+
+    // Nếu ô row + index (Ô đầu tiên của dãy 5) nằm ngoài bàn cờ
+    if (index < 0) {
+      continue;
+    }
+
+    // Trường hợp đủ 5 con trong bàn cờ
+    var isWon = true;
+    
+    
+    for (var i = index; i < index + 5; i++) {
+      
+      if (i > MaxHeight - 1) {
+        isWon = false;
+        break;
+      }
+
+      if (squares[i * MaxWidth + column] !== thisValue) {
+        isWon = false;
+        break;
+      }
+    }
+
+    if (isWon === true) {
+      return thisValue;
+    }
+
+  }
+
+  // // Kiểm tra hàng ngang chứa điểm vừa đánh
+  for (index = column - 4; index <= column; index++) {
+
+    // Nếu ô column + index (Ô đầu tiên của dãy 5) nằm ngoài bàn cờ
+    if (index < 0) {
+      continue;
+    }
+
+    // Trường hợp đủ 5 con trong bàn cờ
+    isWon = true;    
+    for (i = index; i < index + 5; i++) {
+      
+      if (i > MaxWidth - 1) {
+        isWon = false;
+        break;
+      }
+
+      if (squares[row * MaxWidth + i] !== thisValue) {
+        isWon = false;
+        break;
+      }
+    }
+
+    if (isWon === true) {
+      return thisValue;
+    }
+
+  }
+
+  // // Kiểm tra hàng chéo từ trái qua, trên xuống chứa điểm vừa đánh
+  for (index = - 4; index <= 0; index++) {
+
+    // Nếu ô column + index (Ô đầu tiên của dãy 5) nằm ngoài bàn cờ
+    if (index + column < 0 || index + row < 0) {
+      continue;   
+    }
+
+    // Trường hợp đủ 5 con trong bàn cờ
+    isWon = true;    
+    for (i = index; i < index + 5; i++) {
+      
+      if (i + column > MaxWidth - 1 || i + row > MaxHeight - 1) {
+        isWon = false;
+        break;
+      }
+
+      if (squares[(row + i) * MaxWidth + (column + i)] !== thisValue) {
+        isWon = false;
+        break;
+      }
+    }
+
+    if (isWon === true) {
+      return thisValue;
+    }
+
+  }
+
+  // // Kiểm tra hàng chéo từ trái qua, dưới lên chứa điểm vừa đánh
+  for (index = - 4; index <= 0; index++) {
+
+    // Nếu ô column + index (Ô đầu tiên của dãy 5) nằm ngoài bàn cờ
+    if (index + column < 0 || row - index > MaxHeight - 1) {
+      continue;   
+    }
+
+    // Trường hợp đủ 5 con trong bàn cờ
+    isWon = true;    
+    for (i = index; i < index + 5; i++) {
+      
+      if (i + column > MaxWidth - 1 || row - i < 0) {
+        isWon = false;
+        break;
+      }
+
+      if (squares[(row - i) * MaxWidth + (column + i)] !== thisValue) {
+        isWon = false;
+        break;
+      }
+    }
+
+    if (isWon === true) {
+      return thisValue;
+    }
+
+  }
+  return null;
 }
 
 export default Game;
